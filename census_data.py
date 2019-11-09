ba0808a774edcc58dd3998162c8b025fd0ebfbfd
Aleida Olvera <aleidaolvera@gmail.com>
2019-11-09 15:37:12 -0500

    added missing clusters

--- census_data.py
+++ census_data.py
@@ -14,12 +14,12 @@ def collectData():
    # Request 2000 data from API and save it into a csv file
    request = requests.get( urlFor2000 )
    outFile = open( csv2000, 'wt' )
-   outFile.write( request.text )
+   outFile.write( request.text.encode('utf-8') )
    
    # Request 2010 data from API and save it into a csv file
    request = requests.get( urlFor2010 )
    outFile = open( csv2010, 'wt' )
-   outFile.write( request.text )
+   outFile.write( request.text.encode('utf-8'))
    print( outFile )
    
 def openFile( filename ):
@@ -81,8 +81,9 @@ def rename_census_columns(df):
                              "H0010003": "Vacant Housing Units"})
     return df
 
-def bin_census_data(df, cat_name):
-    df['INCOME_LEVEL'] = df[cat_name].apply(lambda x: 'LOW_INCOME' if x <= 30000 else ('MID_INCOME' if x <= 65000 else ('MID_HIGH_INCOME' if x < 100000 else 'HIGH_INCOME')))
+def bin_census_data(df, cat_name, year):
+    income_cat = 'INCOME_LEVEL_' + str(year)
+    df[income_cat] = df[cat_name].apply(lambda x: 'LOW_INCOME' if x <= 30000 else ('MID_INCOME' if x <= 65000 else ('MID_HIGH_INCOME' if x < 100000 else 'HIGH_INCOME')))
 
 def tract_to_cluster(df, cat_name):
     clusters = {
@@ -90,28 +91,28 @@ def tract_to_cluster(df, cat_name):
                 '2': [30, 32, 31, 29, 28.01, 27.01, 27.02, 28.02, 37, 36],
                 '3': [34, 35, 44, 43],
                 '4': [1, 2.02, 2.01],
-                '5': [108, 56, 55],
-                '6': [107, 53.01, 42.02, 42.01],
-                '7': [52.01, 50.01, 50.02, 49.02, 49.01, 48.02, 48.01],
-                '8': [101, 58, 47.01, 47.02, 59],
-                '9': [62.02, 102, 110, 64, 105],
+                '5': [108, 57.02, 57.01, 56, 55],
+                '6': [107, 53.02, 53.01, 54.01, 54.02, 42.02, 42.01],
+                '7': [52.01, 50.01, 50, 50.02, 49.02, 49.01, 48.02, 48.01],
+                '8': [101, 51, 52.02, 58, 47.01, 47, 47.02, 59],
+                '9': [62.01, 62.02, 102, 110, 64, 105, 60.01, 60.02, 63.01, 61],
                 '10': [15, 14.02, 14.01],
                 '11': [11, 10.01],
                 '12': [13.01, 12, 13.02],
                 '13': [8.02, 8.01, 9.01, 9.02],
                 '14': [3, 7.02, 7.01, 10.02],
-                '15': [6, 5.01, 5.02, 4],
+                '15': [6, 5.01, 5.02, 4, 73.02],
                 '16': [16, 103],
-                '17': [17.02, 19.02, 19.01, 18.04, 18.03, 22.01],
+                '17': [17.01, 17.02, 19.02, 19.01, 18.04, 18.03, 22.01],
                 '18': [20.01, 26, 20.02, 21.01, 21.02, 23.01, 24, 25.02, 25.01],
                 '19': [95.05, 95.07, 95.08, 95.01, 22.02],
                 '20': [95.04, 95.09, 95.03],
                 '21': [23.02, 92.01, 92.04, 87.02, 87.01, 46, 33.02, 33.01, 92.03],
-                '22': [93.01, 93.02, 91.02],
+                '22': [93.01, 93.02, 91.02, 91.01],
                 '23': [89.04, 89.03, 88.02, 88.04, 88.03],
                 '24': [94, 111, 90],
-                '25': [106, 84.1, 79.01, 79.03, 80.01, 81, 83.02, 83.01],
-                '26': [68.04, 68.01, 68.02, 69, 67, 82, 80.02, 66, 65, 70, 84.02],
+                '25': [106, 85, 84.1, 79.01, 79.03, 80.01, 81, 83.02, 83.01],
+                '26': [68.04, 68.01, 68.02, 69, 67, 82, 80.02, 66, 65, 70, 84.02, 84.01],
                 '27': [71, 72],
                 '28': [75.03, 75.04],
                 '29': [96.01],
@@ -124,7 +125,7 @@ def tract_to_cluster(df, cat_name):
                 '36': [75.02, 74.08],
                 '37': [74.01, 74.06, 74.07],
                 '38': [73.04, 74.09, 74.03, 74.04],
-                '39': [109, 73.01, 98.07, 98.11, 98.10, 97, 98.02, 98.01, 104, 98.04, 98.03]
+                '39': [109, 73.01, 98.07, 98.11, 98.10, 97, 98.02, 98.08, 98.06, 98.01, 104, 98.04, 98.03]
             }
     # flip keys and values to match tracts with clusters
     tracts = {new_k: old_k for old_k, old_v in clusters.items() for new_k in old_v}
@@ -136,7 +137,7 @@ def extra_clean_up(df, year):
     if year == '2000':
         df['FEDTRACTNO'].round(2)
         tract_to_cluster(df, 'FEDTRACTNO')
-        bin_census_data(df, 'FAGI_MEDIAN_2005')
+        bin_census_data(df, 'FAGI_MEDIAN_2005', '2005')
     if year == '2010':
         df['TRACT'] = df['TRACT'] * float(0.01)
         df['TRACT'] = df['TRACT'].round(2)
@@ -144,7 +145,9 @@ def extra_clean_up(df, year):
         # add pop density, which is present in 2000 census but not in 2010 census
         # formula is total_population / sq_miles
         df['POPDENSITY'] = df['Total Population'] / df['SQ_MILES']
-        bin_census_data(df, 'FAGI_MEDIAN_2015')
+        bin_census_data(df, 'FAGI_MEDIAN_2010', '2010')
+        bin_census_data(df, 'FAGI_MEDIAN_2015', '2015')
+
     
 
 def cleanData():
